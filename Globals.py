1caa7b037fb8e647efa7187ea60b70086f7eb4db
Ilade-s <76600778+Ilade-s@users.noreply.github.com>
2021-12-15 17:01:49 +0100

    version change (v1.1 --> v1.2)

--- Globals.py
+++ Globals.py
@@ -3,7 +3,7 @@ Global variables used in scripts
 """
 FOLDER_PATH = 'archive' # path of the folder to store the mangas folders with the images (can be either relative to the script folder or absolute)
 SIMULTANEOUS_REQUESTS = 10 # should always be under 40 (10 is best)
-__VERSION__ = '1.1'
+__VERSION__ = '1.2'
 __AUTHOR__ = 'Merlet Raphaël'
 def format_title(title: str) -> str:
     """format titles to be usable as filenames and foldernames"""
